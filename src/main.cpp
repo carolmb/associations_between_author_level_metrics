7460f0fbf5681228d13685a55fb3e07fb18e1b78
Ana Caroline <carolmedeirosbrito@gmail.com>
2019-04-02 23:07:15 -0300

    main

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,8 @@ using string = std::string;
 
 int main() {
     igraph_i_set_attribute_table(&igraph_cattribute_table);
-    string filename = "data/citation_net_ge_1990.xnet";
+    // string filename = "data/citation_net_ge_1990.xnet";
+    string filename = "py/colabs/basic_colab_cut/colab_2010_2014_test_0.8_selected_basic.xnet";
     // string filename = "test3.xnet";
     igraph_t g = xnet2igraph(filename);
     
